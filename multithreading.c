36913613f64b54a1f9e6af1474fb38ec2e7ad9f3
dharsan <gdharsan2004dec@gmail.com>
2024-12-27 14:11:18 +0530

    multithreadin

---
+++ multithreading.c
@@ -0,0 +1,32 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void *thread_function(void *args) {
+  int *id = (int *)args;
+  printf("hello linux from %d thread\n", *id);
+  return 0;
+}
+
+int main() {
+  int num_thread = 5;
+  pthread_t threads[num_thread];
+  int thread_id[num_thread];
+
+  // creating threads
+  for (int i = 0; i < num_thread; i++) {
+    thread_id[i] = i + 1;
+    if (pthread_create(&threads[i], NULL, thread_function, &thread_id[i]) !=
+        0) {
+      printf("failed to create a thread");
+      exit(1);
+    }
+  }
+
+  // waiting for threads to complete execution
+  for (int i = 0; i < num_thread; i++) {
+    pthread_join(threads[i], NULL);
+  }
+  
+  return 0;
+}
