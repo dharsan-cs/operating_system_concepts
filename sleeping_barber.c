c7a514bb15ad6256b52db8ce4bf0b3055623ba70
dharsan <gdharsan2004dec@gmail.com>
2024-12-31 11:39:09 +0530

    improved

--- sleeping_barber.c
+++ sleeping_barber.c
@@ -5,65 +5,75 @@
 #include <unistd.h>
 
 typedef struct sleeping_barber {
-  int num_waiting_chair;
   sem_t waiting_chair_sem;
   sem_t haircut_sem;
   sem_t barber_sem;
+  sem_t customer_sem;
 } sleeping_barber;
+
+volatile int running = 1;
+const int num_chairs = 3;
+const int num_customers = 5;
 
 void *customer_thread_func(void *args) {
   sleeping_barber *ptr = (sleeping_barber *)args;
   pthread_t self_id = pthread_self();
 
-  sem_wait(&(ptr->waiting_chair_sem));
-  if (ptr->num_waiting_chair == 0) {
-    printf("%d - exiting\n", (int)self_id);
+  if (sem_trywait(&(ptr->waiting_chair_sem)) == 0) {
+    printf("%d customer - in Waiting_chair\n", (int)self_id);
+    sem_wait(&(ptr->haircut_sem));
     sem_post(&(ptr->waiting_chair_sem));
-    return 0;
+    sem_post(&(ptr->barber_sem));
+    printf("%d customer - in Barber_chair\n", (int)self_id);
+    sem_wait(&(ptr->customer_sem));
+    sem_post(&(ptr->haircut_sem));
+    printf("%d customer - exiting\n", (int)self_id);
   } else {
-    ptr->num_waiting_chair--;
-    sem_post(&(ptr->waiting_chair_sem));
+    printf("%d customer - exiting(no waiting chair)\n", (int)self_id);
   }
-
-  printf("%d - in Waiting_chair\n", (int)self_id);
-  sem_wait(&(ptr->haircut_sem));
-
-  sem_wait(&(ptr->waiting_chair_sem));
-  ptr->num_waiting_chair++;
-  sem_post(&(ptr->waiting_chair_sem));
-
-  sem_post(&(ptr->barber_sem));
-  printf("%d - in Barber_chair\n", (int)self_id);
-  sleep(2);
-
-  sem_post(&(ptr->haircut_sem));
-  return 0;
+  return NULL;
 }
 
 void *barber_thread_func(void *args) {
   sleeping_barber *ptr = (sleeping_barber *)args;
   pthread_t self_id = pthread_self();
-  while (1) {
+  while (running) {
     sem_wait(&(ptr->barber_sem));
-    printf("%d-Barber is cutting hair\n", (int)self_id);
+    if (!running) {
+      break;
+    }
+    // simulating haircut
+    printf("%d Barber is cutting hair\n", (int)self_id);
+    sleep(1);
+    sem_post(&(ptr->customer_sem));
   }
+  printf("%d barber - exiting\n", (int)self_id);
+  return NULL;
 }
 
 int main() {
   sleeping_barber slp_bar;
-
-  slp_bar.num_waiting_chair = 3;
-  if (sem_init(&slp_bar.haircut_sem, 0, 1) != 0 ||
-      sem_init(&slp_bar.waiting_chair_sem, 0, 1) != 0) {
+  if (sem_init(&slp_bar.haircut_sem, 0, 1) != 0) {
     perror("Sem init failed");
     exit(EXIT_FAILURE);
   }
+
+  if (sem_init(&slp_bar.waiting_chair_sem, 0, num_chairs) != 0) {
+    perror("Sem init failed");
+    exit(EXIT_FAILURE);
+  }
+
   if (sem_init(&slp_bar.barber_sem, 0, 0) != 0) {
     perror("Sem init failed");
     exit(EXIT_FAILURE);
   }
 
-  pthread_t customer_thread[5], barber_thread;
+  if (sem_init(&slp_bar.customer_sem, 0, 0) != 0) {
+    perror("Sem init failed");
+    exit(EXIT_FAILURE);
+  }
+
+  pthread_t customer_thread[num_customers], barber_thread;
   pthread_create(&barber_thread, NULL, barber_thread_func, &slp_bar);
   for (int i = 0; i < 5; i++) {
     pthread_create(&customer_thread[i], NULL, customer_thread_func, &slp_bar);
@@ -71,5 +81,8 @@ int main() {
   for (int i = 0; i < 5; i++) {
     pthread_join(customer_thread[i], NULL);
   }
-  pthread_cancel(barber_thread);
+  running = 0;
+  sem_post(&slp_bar.barber_sem);
+  pthread_join(barber_thread, NULL);
+  
 }
